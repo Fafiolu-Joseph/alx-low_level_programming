f4d70fb71b16a8836d5ddf59495707bae8be9e46
Fafiolu Joseph <fafiolujoseph@gmail.com>
2022-10-14 15:12:23 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints lowercase alphabets
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+		else
+		{
+		}
+	}
+	putchar('\n');
+	return (0);
+}
