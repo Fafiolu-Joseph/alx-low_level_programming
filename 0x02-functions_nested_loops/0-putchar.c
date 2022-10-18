7d1334dce51cbd4b6c0738a0f736e7dab96cdb4f
Fafiolu Joseph <fafiolujoseph@gmail.com>
2022-10-18 10:37:57 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry Point
+ * Description: Printing with _putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	char c[] = "_printchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
