249dc9ae467b1f6ec758120e72a0810308a6c07d
Fafiolu Joseph <fafiolujoseph@gmail.com>
2022-10-18 14:25:28 -0700

    Absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value of a number
+ * @n : number to check
+ * Return: 0 or 1
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		n = n;
+	}
+	else
+	{
+		n = n * -1;
+	}
+}
