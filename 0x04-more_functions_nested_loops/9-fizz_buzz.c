c1f27844f6c13bef074ef952b947a7e53fc2c174
Fafiolu Joseph <fafiolujoseph@gmail.com>
2022-10-20 16:44:05 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Fizz - Buzz
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, n;
+
+	n = 100;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d%", i);
+		}
+		putchar(' ');
+	}
+}
