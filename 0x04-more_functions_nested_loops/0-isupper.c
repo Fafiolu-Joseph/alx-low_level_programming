1328a79d49d0d6122c986cbf44339df3d91359ed
Fafiolu Joseph <fafiolujoseph@gmail.com>
2022-10-20 14:24:59 -0700

    Isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,22 @@
 #include "main.h"
+
 /**
  * _isupper - check for uppercase letter
  * @c: value for checking
  * Return: Always 0
  */
+
 int _isupper(int c)
 {
 	int rval;
 
 	if (c >= 'A' && c <= 'Z')
 	{
-		rval = 1;
+		return (1);
 	}
 	else
 	{
-		rval = 0;
+		return (0);
 	}
 
-	return (rval);
 }
