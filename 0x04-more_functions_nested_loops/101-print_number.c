75ddd61ac98588e7a7c9d2a234371af0f43aa8df
Fafiolu Joseph <fafiolujoseph@gmail.com>
2022-10-20 17:36:58 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+/**
+ * print_number - print numbers
+ * @n: numbers to be printed
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int a = 0;
+
+	if (n < 0)
+	{
+		a = -n;
+		_putchar('-');
+	}
+	else
+	{
+		a = n;
+	}
+
+	if (a / 10)
+	{
+		print_number(a / 10);
+	}
+
+	_putchar((a % 10) + '0');
+}
